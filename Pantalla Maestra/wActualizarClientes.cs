e0ad56a0550992200ebab9beeba5ee62fafdbb51
Redienx <117588167+Redienx@users.noreply.github.com>
2023-05-17 01:52:45 -0500

    Falta Navegacion
    La navegación no supe como hacerla

---
+++ Pantalla Maestra/wActualizarClientes.cs
@@ -0,0 +1,85 @@
+﻿using Finisar.SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pantalla_Maestra
+{
+    public partial class fmrActualizarClientes : Form
+    {
+        string Nombre, Apellido, Celular, Correo;
+        int Edad, id;
+
+
+        private void txtEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        public fmrActualizarClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnActualizarClientes_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection Conexion_sqlite;
+            SQLiteCommand cmd_sqlite;
+
+            Conexion_sqlite = new SQLiteConnection("Data Source=dbMercado.db;Version = 3; Compress= True");
+            try
+            {
+                Conexion_sqlite.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se encontro base de datos");
+            }
+
+            cmd_sqlite = Conexion_sqlite.CreateCommand();
+            try
+            {
+                id = int.Parse(txtid.Text);
+                Nombre = txtNombre.Text;
+                Apellido = txtApellido.Text;
+                Edad = int.Parse(txtEdad.Text);
+                Celular = txtCelular.Text;
+                Correo = txtCorreo.Text;
+            } catch (Exception ex) { MessageBox.Show("Las cajas de texto no pueden estar vacias"); }
+            cmd_sqlite.CommandText = $"UPDATE tblClientes SET Nombre = '{Nombre}', Apellido = '{Apellido}', Edad = '{Edad}', Celular = '{Celular}', Correo = '{Correo}' WHERE ID = '{id}';";
+            cmd_sqlite.ExecuteNonQuery();
+
+            Conexion_sqlite.Close();
+
+            txtid.Text = null;
+            txtNombre.Text = null;
+            txtApellido.Text = null;
+            txtEdad.Text = null;
+            txtCelular.Text = null;
+            txtCorreo.Text = null;
+
+            this.Close();
+        }
+
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtid.Text = null;
+            txtNombre.Text = null;
+            txtApellido.Text = null;
+            txtEdad.Text = null;
+            txtCelular.Text = null;
+            txtCorreo.Text = null;
+            this.Close();
+        }
+    }
+}
---
+++ Pantalla Maestra/wTablaClientes.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Finisar.SQLite;
+
+
+namespace Pantalla_Maestra
+{
+    public partial class fmrTablaClientes : Form
+    {
+        string Nombre, Apellido, Celular, Correo;
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            fmrBorrarCliente fmrBorrarCliente = new fmrBorrarCliente(); 
+            fmrBorrarCliente.Show();
+        }
+
+        private void fmrTablaClientes_Load(object sender, EventArgs e)
+        {
+            SQLiteConnection Conexion_sqlite;
+
+            Conexion_sqlite = new SQLiteConnection("Data Source=dbMercado.db;Version = 3; Compress= True");
+            try
+            {
+                Conexion_sqlite.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se encontro base de datos");
+            }
+
+            string Consulta = "SELECT * from tblClientes";
+            SQLiteDataAdapter dataAdapter_sqlite = new SQLiteDataAdapter(Consulta, Conexion_sqlite);
+            DataTable dt = new DataTable();
+            dataAdapter_sqlite.Fill(dt);
+            dtgUsuarios.DataSource = dt;
+        }
+
+        private void btnActualizarBase_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection Conexion_sqlite;
+
+            Conexion_sqlite = new SQLiteConnection("Data Source=dbMercado.db;Version = 3; Compress= True");
+            try
+            {
+                Conexion_sqlite.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se encontro base de datos");
+            }
+
+            string Consulta = "SELECT * from tblClientes";
+            SQLiteDataAdapter dataAdapter_sqlite = new SQLiteDataAdapter(Consulta, Conexion_sqlite);
+            DataTable dt = new DataTable();
+            dataAdapter_sqlite.Fill(dt);    
+            dtgUsuarios.DataSource = dt;
+            
+            Conexion_sqlite.Close();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection Conexion_sqlite;
+
+            Conexion_sqlite = new SQLiteConnection("Data Source=dbMercado.db;Version = 3; Compress= True");
+            try
+            {
+                Conexion_sqlite.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se encontro base de datos");
+            }
+            if (rbnid.Checked == true)
+            {
+                try
+                {
+                    string Consulta = "SELECT * from tblClientes WHERE ID = " + txtBuscar.Text + "";
+                    SQLiteDataAdapter dataAdapter_sqlite = new SQLiteDataAdapter(Consulta, Conexion_sqlite);
+                    DataTable dt = new DataTable();
+                    dataAdapter_sqlite.Fill(dt);
+                    dtgUsuarios.DataSource = dt;
+                    SQLiteCommand cmd_sqlite = new SQLiteCommand(Consulta, Conexion_sqlite);
+                    SQLiteDataReader dataReader_sqlite = cmd_sqlite.ExecuteReader();
+                }catch (Exception ex) { MessageBox.Show("Debe de ingresar un valor"); }
+                Conexion_sqlite.Close();
+                
+            }
+            else if (rbnNombre.Checked == true)
+            {
+                try
+                {
+                    string Consulta = "SELECT * from tblClientes WHERE Nombre = '" + txtBuscar.Text + "'";
+                    SQLiteDataAdapter dataAdapter_sqlite = new SQLiteDataAdapter(Consulta, Conexion_sqlite);
+                    DataTable dt = new DataTable();
+                    dataAdapter_sqlite.Fill(dt);
+                    dtgUsuarios.DataSource = dt;
+                    SQLiteCommand cmd_sqlite = new SQLiteCommand(Consulta, Conexion_sqlite);
+                    SQLiteDataReader dataReader_sqlite = cmd_sqlite.ExecuteReader();
+                }
+                catch (Exception ex) { MessageBox.Show("Debe de ingresar un valor"); }
+
+                Conexion_sqlite.Close();
+
+            }
+            else if (rbnApellido.Checked == true)
+            {
+                try
+                {
+                    string Consulta = "SELECT * from tblClientes WHERE Apellido = '" + txtBuscar.Text + "'";
+                    SQLiteDataAdapter dataAdapter_sqlite = new SQLiteDataAdapter(Consulta, Conexion_sqlite);
+                    DataTable dt = new DataTable();
+                    dataAdapter_sqlite.Fill(dt);
+                    dtgUsuarios.DataSource = dt;
+                    SQLiteCommand cmd_sqlite = new SQLiteCommand(Consulta, Conexion_sqlite);
+                    SQLiteDataReader dataReader_sqlite = cmd_sqlite.ExecuteReader();
+                }
+                catch (Exception ex) { MessageBox.Show("Debe de ingresar un valor"); }
+
+                Conexion_sqlite.Close();
+
+            }
+            else if (rbnEdad.Checked == true)
+            {
+                try
+                {
+                    string Consulta = "SELECT * from tblClientes WHERE Edad = " + txtBuscar.Text + "";
+                    SQLiteDataAdapter dataAdapter_sqlite = new SQLiteDataAdapter(Consulta, Conexion_sqlite);
+                    DataTable dt = new DataTable();
+                    dataAdapter_sqlite.Fill(dt);
+                    dtgUsuarios.DataSource = dt;
+                    SQLiteCommand cmd_sqlite = new SQLiteCommand(Consulta, Conexion_sqlite);
+                    SQLiteDataReader dataReader_sqlite = cmd_sqlite.ExecuteReader();
+                }
+                catch (Exception ex) { MessageBox.Show("Debe de ingresar un valor"); }
+
+                Conexion_sqlite.Close();
+
+            }
+            else if (rbnCelular.Checked == true)
+            {
+                try
+                {
+                    string Consulta = "SELECT * from tblClientes WHERE Celular = '" + txtBuscar.Text + "'";
+                    SQLiteDataAdapter dataAdapter_sqlite = new SQLiteDataAdapter(Consulta, Conexion_sqlite);
+                    DataTable dt = new DataTable();
+                    dataAdapter_sqlite.Fill(dt);
+                    dtgUsuarios.DataSource = dt;
+                    SQLiteCommand cmd_sqlite = new SQLiteCommand(Consulta, Conexion_sqlite);
+                    SQLiteDataReader dataReader_sqlite = cmd_sqlite.ExecuteReader();
+                }
+                catch (Exception ex) { MessageBox.Show("Debe de ingresar un valor"); }
+
+                Conexion_sqlite.Close();
+
+            }
+            else if (rbnCorreo.Checked == true)
+            {
+                try
+                {
+                    string Consulta = "SELECT * from tblClientes WHERE Correo = '" + txtBuscar.Text + "'";
+                    SQLiteDataAdapter dataAdapter_sqlite = new SQLiteDataAdapter(Consulta, Conexion_sqlite);
+                    DataTable dt = new DataTable();
+                    dataAdapter_sqlite.Fill(dt);
+                    dtgUsuarios.DataSource = dt;
+                    SQLiteCommand cmd_sqlite = new SQLiteCommand(Consulta, Conexion_sqlite);
+                    SQLiteDataReader dataReader_sqlite = cmd_sqlite.ExecuteReader();
+                }
+                catch (Exception ex) { MessageBox.Show("Debe de ingresar un valor"); }
+
+                Conexion_sqlite.Close();
+
+            }
+            else { MessageBox.Show("Seleccione una opcion."); }
+        }
+
+        public fmrTablaClientes()
+        {
+            InitializeComponent();
+        }
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            fmrCrearClientes fmrCrearClientes = new fmrCrearClientes();
+            fmrCrearClientes.Show();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            fmrActualizarClientes fmrActualizarClientes = new fmrActualizarClientes();
+            fmrActualizarClientes.Show();
+        }
+    }
+}
